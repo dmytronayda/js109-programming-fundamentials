b72123c993f4a6688ae64bca01a711125bc1b480
dmytronayda <dima.nayda.1993@gmail.com>
2020-06-29 06:18:12 +0200

    leapYears problem part 1 & 2.

--- take2/leapYears.js
+++ take2/leapYears.js
@@ -11,28 +11,32 @@ Examples:
 - isLeapYear(1000); // 1000 % 400 => false = 1000 % 100 => true 
 
 Algorithm:
+- check if number is < 1752 => if it is check if number % 4 evenly => if yes, return true
 - check if number is divisible by 400 evenly => if yes return true
 - if not, check if number is divisible by 100 evenly if yes return false
 - if not, check if number is divisible by 4 evenly if yes return true
 */
 
 function isLeapYear(year) {
+  if (year < 1752) {
+    if (year % 4 === 0) return true;
+  }
   if (year % 400 === 0) return true;
   if (year % 100 === 0) return false;
   if (year % 4 === 0) return true; 
   return false;
 }
 
-console.log(isLeapYear(2016));      // true
-console.log(isLeapYear(2015));      // false
-console.log(isLeapYear(2100));      // false
-console.log(isLeapYear(2400));      // true
-console.log(isLeapYear(240000));    // true
-console.log(isLeapYear(240001));    // false
-console.log(isLeapYear(2000));      // true
-console.log(isLeapYear(1900));      // false
-console.log(isLeapYear(1752));      // true
-console.log(isLeapYear(1700));      // false
-console.log(isLeapYear(1));         // false
-console.log(isLeapYear(100));       // false
-console.log(isLeapYear(400));       // true
+console.log(isLeapYear(2016) === true);      
+console.log(isLeapYear(2015) === false);    
+console.log(isLeapYear(2100) === false);     
+console.log(isLeapYear(2400) === true);      
+console.log(isLeapYear(240000) === true);    
+console.log(isLeapYear(240001) === false);  
+console.log(isLeapYear(2000) === true);     
+console.log(isLeapYear(1900) === false);    
+console.log(isLeapYear(1752) === true);     
+console.log(isLeapYear(1700) === true);     
+console.log(isLeapYear(1) === false);       
+console.log(isLeapYear(100) === true);     
+console.log(isLeapYear(400) === true);     

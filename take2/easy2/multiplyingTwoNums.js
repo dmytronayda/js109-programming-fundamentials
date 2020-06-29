fdd3b099c1fc11f6270251c33d38622d35f87cee
dmytronayda <dima.nayda.1993@gmail.com>
2020-06-29 08:08:28 +0200

    Add multiplyingTwoNums problem solution.

---
+++ take2/easy2/multiplyingTwoNums.js
@@ -0,0 +1,23 @@
+/*
+Notes:
+- input is 2 number arguments
+- output is a product of 2 arguments' multiplication
+
+Examples:
+console.log(multiply(5, 2));  // 10
+console.log(multiply(5, 5));  // 25
+console.log(multiply(0, 5));  // 0
+console.log(multiply(-1, 5)); // -5
+
+Algorithm:
+- return num1 * num2
+*/
+
+let multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+console.log(multiply(5, 2));  // 10
+console.log(multiply(5, 5));  // 25
+console.log(multiply(0, 5));  // 0
+console.log(multiply(-1, 5)); // -5
